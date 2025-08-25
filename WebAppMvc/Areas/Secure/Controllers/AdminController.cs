b6ddad1eb806ef9905d56145f9f84d67367a3c1f
arvind-sandesh <arvind-sandesh@outlook.com>
2025-08-25 18:02:23 +0530

    edit updated

--- WebAppMvc/Areas/Secure/Controllers/AdminController.cs
+++ WebAppMvc/Areas/Secure/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppMvc.Data;
+using WebAppMvc.Models.DTOs;
 
 namespace WebAppMvc.Areas.Secure.Controllers
 {
@@ -7,9 +11,41 @@ namespace WebAppMvc.Areas.Secure.Controllers
     [Authorize(Roles = "SuperAdmin,Admin")]
     public class AdminController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult AddInformation()
+        {
+            var InfoTypeList = _context.InfoTypes.Where(x => x.IsActive == true).Select(x => new SelectListItem() { Text = x.InfoTypeName, Value = x.Id.ToString() }).ToList();
+            var FacultyList = _context.Faculties.Where(x => x.IsActive == true).Select(x => new SelectListItem() { Text = x.FacultyName, Value = x.Id.ToString() }).ToList();
+            ViewBag.InfoTypeId = InfoTypeList;
+            ViewBag.FacultyId = InfoTypeList;
+            return View();
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddInformation(AddInfoDTO objEnt)
+        {
+            var InfoTypeList = _context.InfoTypes.Where(x => x.IsActive == true).Select(x => new SelectListItem() { Text = x.InfoTypeName, Value = x.Id.ToString() }).ToList();
+            var FacultyList = _context.Faculties.Where(x => x.IsActive == true).Select(x => new SelectListItem() { Text = x.FacultyName, Value = x.Id.ToString() }).ToList();
+            ViewBag.InfoTypeId = InfoTypeList;
+            ViewBag.FacultyId = InfoTypeList;
+            if (ModelState.IsValid)
+            { 
+
+            }
+            return View();
+
+        }
+
     }
 }
---
+++ WebAppMvc/Models/DTOs/AddInfoDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebAppMvc.Domain.Entities;
+
+namespace WebAppMvc.Models.DTOs
+{
+    public class AddInfoDTO
+    {
+        [Required]
+        [Display(Name ="Category")]
+        public int InfoTypeId { get; set; }
+        [Display(Name = "Faculty")]
+        public int? FacultyId { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(2000)]
+        [Display(Name = "Description")]
+        public string? Content { get; set; } = string.Empty;        
+
+        [StringLength(500)]
+        public string? WebUrl { get; set; }        
+        
+        [NotMapped]
+        public IFormFile? Attachment { get; set; }
+        
+        [Required]
+        [Display(Name = "Create Date")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow.AddHours(5).AddMinutes(30);
+
+    }
+}
